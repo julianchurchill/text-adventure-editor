ff0e7037129cf67b84e599a3218d9fbb59512e26
Julian Churchill <chewielouie03@gmail.com>
2013-03-03 14:15:46 +0000

    Initial Lein generated project skeleton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to text-adventure-editor
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/text_adventure_editor/core_test.clj
@@ -0,0 +1,7 @@
+(ns text-adventure-editor.core-test
+  (:use clojure.test
+        text-adventure-editor.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
