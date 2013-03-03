da22d35a849b30bca216309e32d55bd4fa8f9a76
Julian Churchill <chewielouie03@gmail.com>
2013-03-03 21:08:00 +0000

    Updated plugins

--- project.clj
+++ project.clj
@@ -1,21 +1,25 @@
 (defproject text-adventure-editor "0.1.0-SNAPSHOT"
-            :description "A self-hosted web app editor for my text adventure game"
-            :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.3.0-beta3"]
-                           [monet "0.1.0-SNAPSHOT"]
-                           [jayq "0.1.0-SNAPSHOT"]]
+  :description "A self-hosted web app editor for my text adventure game"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [noir "1.3.0-beta3"]
+                 [monet "0.1.0-SNAPSHOT"]
+                 ;                           [waltz "0.1.0-SNAPSHOT"]
+                 ;                 					 [fetch "0.1.0-SNAPSHOT"]
+                 [jayq "2.3.0"]]
 
-  					;; Add lein-cljsbuild plugin
-            :plugins [[lein-cljsbuild "0.2.8"]]
+  ;; Add lein-cljsbuild plugin
+  :plugins [[lein-cljsbuild "0.3.0"]]
             
-            ;; config. for cljsbuild
-            :cljsbuild {
-                        :builds [{
-                                  :source-path "src/textadventureeditor/client"
-                                  :compiler {
-                                             :output-to "resources/public/js/main.js"
-                                             :optimizations :whitespace
-                                             :pretty-print true}}]}
-  					
-  					:main textadventureeditor.server)
+  ;; config. for cljsbuild
+  :cljsbuild {:builds
+              [{:source-paths ["src/textadventureeditor/client"],
+                :compiler
+                {:output-to "resources/public/js/main.js",
+                 :optimizations :whitespace,
+                 ;need this if optimization == advanced
+                 ; also need to include jquery.js in our source
+                 ;:externs ["externs/jquery.js"],
+                 :pretty-print true}}]}
+  
+  :main textadventureeditor.server)
 
