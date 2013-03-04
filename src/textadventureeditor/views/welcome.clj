3a2e2b628c9d7b6ef7e3b060974fe0881c39be39
Julian Churchill <chewielouie03@gmail.com>
2013-03-04 20:16:09 +0000

    Added id and description text fields

--- src/textadventureeditor/views/welcome.clj
+++ src/textadventureeditor/views/welcome.clj
@@ -1,13 +1,21 @@
 (ns textadventureeditor.views.welcome
   (:require [textadventureeditor.views.common :as common])
   (:use [noir.core :only [defpage defpartial]]
+        [hiccup.form :only [form-to text-field]]
         [hiccup.page :only [html5]]))
 
-(defpartial editor []
+(defpartial properties [loc]
+  [:div#properties
+   (form-to [:post loc]
+    (text-field "location id" "no id")
+    (text-field "location description" "no description"))])
+
+(defpartial editor [loc]
   [:div#editor
+   (properties loc)
    [:canvas#canvas {:width 800 :height 650 :tabindex 1}]])
 
 (defpage "/" []
          (common/layout
            [:p "Welcome to text-adventure-editor"]
-          (editor)))
+          (editor "/submit")))
